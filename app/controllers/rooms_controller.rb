96def8e3c74dabe874cb3fd8012b2eafa6cca58f
ishikawa096 <ishikawa096@gmail.com>
2022-02-17 22:36:38 +0900

    ルーム登録機能を作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,9 +9,10 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
+
      if @room.save
       flash[:notice] = "ルームを新規登録しました"
-      redirect_to :roots
+      redirect_to :root
     else
       render "new"
     end
@@ -33,6 +34,6 @@ class RoomsController < ApplicationController
     private
 
   def room_params
-    params.require(:room).permit(:room_name, :room_introduction, :room_image, :room_rate, :room_address)
+    params.require(:room).permit(:room_name, :room_introduction, :room_image, :room_rate, :room_address, :user_id)
   end
 end
