d030f627c9c94225622756b4f35ffdb0589f6fc1
ishikawa096 <ishikawa096@gmail.com>
2022-03-01 21:22:57 +0900

    ユーザー作成時、自己紹介を自動入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,22 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_one_attached :icon
 
+  before_validation :default_introduction
+
   validates :name, presence: true, length: { maximum: 25 }
   validates :introduction, presence: true, length: { maximum: 200 }
   validates :email, presence: true, uniqueness: true
 
   before_create :default_icon
+
+  private
+
+  def default_introduction
+    if self.introduction.blank?
+      self.introduction = "よろしくお願いします"
+    end
+  end
+
   def default_icon
     if !self.icon.attached?
       self.icon.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_icon.jpg')), filename: 'default_icon.jpg', content_type: 'image/jpg')
